413733d02985f8731bdd26fb656e0efeb71e4d72
andem25 <andreamatticola01@gmail.com>
2025-07-11 10:36:37 +0200

    EXAM

--- config/settings.py
+++ config/settings.py
@@ -18,10 +18,10 @@ if not NGROK_TOKEN:
 else:
     print("NGROK_TOKEN found.")
 
-MQTT_BROKER = os.getenv("MQTT_BROKER", "f1d601b5c9184556910bdc2b4e6dfe17.s1.eu.hivemq.cloud")
+MQTT_BROKER = os.getenv("MQTT_BROKER", "DEFAULT_MQTT_BROKER")
 MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
-MQTT_USERNAME = os.getenv("MQTT_USERNAME", "test0")
-MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "Prova12345")
+MQTT_USERNAME = os.getenv("MQTT_USERNAME", "DEFAULT_MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "DEFAULT_MQTT_PASSWORD")
 
 
 # Server Configuration
@@ -56,7 +56,6 @@ MQTT_TOPIC_DOOR = os.getenv("MQTT_TOPIC_DOOR", "door")
 MQTT_TOPIC_EMERGENCY = os.getenv("MQTT_TOPIC_EMERGENCY", "emergency")
 MQTT_TOPIC_ENVIRONMENTAL = os.getenv("MQTT_TOPIC_ENVIRONMENTAL", "environmental_data")
 MQTT_TOPIC_ASSOC = os.getenv("MQTT_TOPIC_ASSOC", "assoc")
-MQTT_TOPIC_LED_STATES = os.getenv("MQTT_TOPIC_LED_STATES", "all_devices/led_states")
 MQTT_TOPIC_NOTIFICATION = os.getenv("MQTT_TOPIC_NOTIFICATION", "notification")
 
 # Debug: stampa i valori letti per verifica
